21a17c73733d7b9c8efe414613f5879991c44227
Simo Naatula <git@naatu.la>
2019-02-09 16:38:47 +0200

    Redo calendar layout

--- calendar.php
+++ calendar.php
@@ -13,9 +13,11 @@
   <link rel="stylesheet" type="text/css" href="infotv.css">
 </head>
 <body id="calendar">
-  <a href="./">
-    <iframe src="https://www.google.com/calendar/embed?src=8khe2ngv0i2rvgpn58s20i331s%40group.calendar.google.com&ctz=Europe/Helsinki"></iframe>
-    <div id="calendar_bar">Paina mistä tahansa palataksesi</div>
-  </a>
+    <iframe src="https://www.google.com/calendar/embed?src=8khe2ngv0i2rvgpn58s20i331s%40group.calendar.google.com&ctz=Europe/Helsinki&hl=fi"></iframe>
+    <div id="calendar_bar"></div>
+    <div id="calendar_noclick"></div>
+    <a href="./">
+      <div class="close material-icons">close</div>
+    </a>
 </body>
 </html>
