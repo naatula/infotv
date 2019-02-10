ad037cc67d17d7f2ffacae19a4128591545bbfe5
Simo Naatula <git@naatu.la>
2019-02-10 11:26:54 +0200

    Add optional seconds and visual improvements to clock view

--- clock.php
+++ clock.php
@@ -14,11 +14,26 @@
 
   <script>
     function toggleWlan() {
-      var x = document.getElementById("wlan");
+      var info = document.getElementById("wlan");
+      var icon = document.getElementById("wlan_toggle");
+      if (info.style.display === "none") {
+        info.style.display = "block";
+        icon.innerHTML = "wifi";
+      } else {
+        info.style.display = "none";
+        icon.innerHTML = "wifi_off";
+      }
+    }
+
+    function toggleSeconds() {
+      var x = document.getElementById("seconds");
+      var icon = document.getElementById("seconds_toggle");
       if (x.style.display === "none") {
-        x.style.display = "block";
+        x.style.display = "inline";
+        icon.innerHTML = "timer";
       } else {
         x.style.display = "none";
+        icon.innerHTML = "timer_off";
       }
     }
 
@@ -26,10 +41,14 @@
       var today = new Date();
       var h = today.getHours();
       var m = today.getMinutes();
+      var s = today.getSeconds();
       h = checkTime(h);
       m = checkTime(m);
+      s = checkTime(s);
       document.getElementById('time').innerHTML =
       h + ":" + m;
+      document.getElementById('seconds').innerHTML =
+      ":" + s;
       var t = setTimeout(startTime, 1000);
     }
     function checkTime(i) {
@@ -47,7 +66,7 @@
           <div id="wlan" style="display: none;">
           <div id="ssid">espoo_training</div><div id="pass">OLab16#xT5n</div>
           </div>
-          <div id="time"></div>
+          <div id="time"></div><div id="seconds" style="display: none;"></div>
         </td>
       </tr>
     </table>
@@ -56,8 +75,12 @@
     <a href="./">
       <div class="close material-icons button">close</div>
     </a>
-    <a href="#">
-      <div class="bottom_right material-icons button" onclick="toggleWlan()">wifi</div>
-    </a>
+      <div class="bottom_right">
+        <a href="#">
+          <div id="wlan_toggle" class="material-icons button" onclick="toggleWlan()">wifi_off</div></a>
+        <a href="#">
+          <div id="seconds_toggle" class="material-icons button" onclick="toggleSeconds()">timer_off</div>
+        </a>
+      </div>
 </body>
 </html>
