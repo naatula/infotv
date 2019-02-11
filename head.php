eb585fc5e63116d090f74ea40e635527fe083fad
Simo Naatula <git@naatu.la>
2019-02-11 10:04:56 +0200

    Add functionality to new weather view and fix Material Icons

--- head.php
+++ head.php
@@ -1,6 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <link rel="manifest" href="infotv.webmanifest">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="infotv.css">
 
 <?php
--- weather.php
+++ weather.php
@@ -7,6 +7,43 @@
 
 </head>
 
+<?php
+  $xml = simplexml_load_file("temp/weather.xml");
+
+  $temp_out = $xml->temp_c;
+  $temp_out_max = round(($xml->davis_current_observation->temp_day_high_f - 32) * (5/9),1);
+#  $temp_out_max_time = $xml->davis_current_observation->temp_day_high_time;
+  $temp_out_min = round(($xml->davis_current_observation->temp_day_low_f - 32) * (5/9),1);
+#  $temp_out_min_time = $xml->davis_current_observation->temp_day_low_time;
+  $temp_in = round(($xml->davis_current_observation->temp_in_f - 32) * (5/9),1);
+  $temp_in_max = round(($xml->davis_current_observation->temp_in_day_high_f - 32) * (5/9),1);
+#  $temp_in_max_time = $xml->davis_current_observation->temp_in_day_high_time;
+  $temp_in_min = round(($xml->davis_current_observation->temp_in_day_low_f - 32) * (5/9),1);
+#  $temp_in_min_time = $xml->davis_current_observation->temp_in_day_low_time;
+  $feels_like = $xml->heat_index_c;
+  $wind = round($xml->wind_mph*0.44704);
+  $wind_dir = $xml->wind_degrees;
+  $gust = round($xml->davis_current_observation->wind_day_high_mph*0.44704);
+  $humidity_out = $xml->relative_humidity;
+  $dewpoint = $xml->dewpoint_c;
+  $rain = round($xml->davis_current_observation->rain_day_in * 25.4,1);
+  $rain_rate = round($xml->davis_current_observation->rain_rate_in_per_hr * 25.4,2);
+  $pressure = $xml->pressure_mb;
+  $pressure_tendency = $xml->davis_current_observation->pressure_tendency_string;
+  $pressure_trend = array(
+    "Steady"=>"tasainen",
+    "Rising Slowly"=>"nousee hitaasti",
+    "Falling Slowly"=>"laskee hitaasti",
+  )[strval($pressure_tendency)];
+
+  $solar_radiation = round($xml->davis_current_observation->solar_radiation);
+  $solar_radiation_max = round($xml->davis_current_observation->solar_radiation_month_high);
+  $uv_index = $xml->davis_current_observation->uv_index;
+  $sunset = $xml->davis_current_observation->sunset;
+  $update_time = $xml->observation_time_rfc822;
+
+ ?>
+
 <body id="weather">
     <table class="title">
       <tr>
@@ -14,42 +51,50 @@
       </tr>
       <tr>
         <td id="outside" class="temperature">
-          <span class="material-icons">landscape</span>
-          <div class="value">0,0<span class="unit">°C</span></div>
+          <i class="material-icons">&#xe564;</i>
+          <div class="value"><?php echo $temp_out ?><span class="unit">°C</span></div>
           <div class="highlow">
             <table>
             <tr>
               <td>
-                <span class="material-icons">whatshot</span>
-                <span class="value">2,5°</span>
+                <i class="material-icons">&#xe80e;</i>
+                <span class="value"><?php echo $temp_out_max ?><span class="unit">°C</span></span>
               </td>
-              <td>04:34</td></tr>
+              <td>
+                <i class="material-icons">&#xe3ab;</i>
+                <span class="value"><?php echo $sunset ?></span>
+              </td>
+            </tr>
             <tr>
               <td>
-                <span class="material-icons">ac_unit</span>
-                <span class="value">-1,3°</span>
+                <i class="material-icons">&#xEB3B;</i>
+                <span class="value"><?php echo $temp_out_min ?><span class="unit">°C</span></span>
               </td>
-              <td>15:44</td></tr>
+              <td>
+                <i class="material-icons">&#xe7fd;</i>
+                <span class="value"><?php echo $feels_like ?><span class="unit">°C</span></span>
+              </td>
+            </tr>
             </table>
           </div>
         </td>
         <td id="inside" class="temperature">
-          <span class="material-icons">home</span>
-          <div class="value">21,5<span class="unit">°C</span></div>
+          <i class="material-icons">&#xe88a;</i>
+          <div class="value"><?php echo $temp_in ?><span class="unit">°C</span></div>
           <div class="highlow">
             <table>
             <tr>
               <td>
-                <span class="material-icons">whatshot</span>
-                <span class="value">22,5°</span>
+                <i class="material-icons">&#xe80e;</i>
+                <span class="value"><?php echo $temp_in_max ?><span class="unit">°C</span></span>
               </td>
-              <td>16:35</td></tr>
+            </tr>
             <tr>
               <td>
-                <span class="material-icons">ac_unit</span>
-                <span class="value">19,3°</span>
+                <i class="material-icons">&#xEB3B;</i>
+                <span class="value"><?php echo $temp_in_min ?><span class="unit">°C</span></span>
               </td>
-              <td>02:44</td></tr>
+            </tr>
             </table>
           </div>
         </td>
@@ -61,9 +106,9 @@
           Tuuli:
         </td>
         <td>
-          10<span class="unit">m/s</span><span class="extra">240°</span>
+          <?php echo $wind ?><span class="unit">m/s</span><span class="extra"><?php echo $wind_dir ?>°</span>
         </td>
-        <td>puuska tänään 16<span class="unit">m/s</span></td>
+        <td>puuska tänään <?php echo $gust ?><span class="unit">m/s</span></td>
       </tr>
 
       <tr>
@@ -71,9 +116,9 @@
           Ilmankosteus:
         </td>
         <td>
-          100<span class="unit">%</span>
+          <?php echo $humidity_out ?><span class="unit">%</span>
         </td>
-        <td>kastepiste 2,0<span class="unit">°C</span></td>
+        <td>kastepiste <?php echo $dewpoint ?><span class="unit">°C</span></td>
       </tr>
 
       <tr>
@@ -81,9 +126,18 @@
           Sademäärä:
         </td>
         <td>
-          10<span class="unit">mm</span>
+          <?php echo $rain ?><span class="unit">mm</span>
         </td>
-        <td>tällä hetkellä ei sada</td>
+        <td>
+          <?php
+          if (!$rain_rate){
+            echo "ei sada";
+          } else {
+            echo "sataa " . $rain_rate . '<span class="unit">mm/h</span>';
+          }
+          ?>
+
+        </td>
       </tr>
 
       <tr>
@@ -91,9 +145,9 @@
           Ilmanpaine:
         </td>
         <td>
-          1000.0<span class="unit">hPa</span>
+          <?php echo $pressure ?><span class="unit">hPa</span>
         </td>
-        <td>kasvaa</td>
+        <td><?php echo $pressure_trend ?></td>
       </tr>
 
       <tr>
@@ -101,9 +155,9 @@
           Auringon säteily:
         </td>
         <td>
-          10<span class="unit">W/m²</span>
+          <?php echo $solar_radiation ?><span class="unit">W/m²</span>
         </td>
-        <td>suurin tänään 100<span class="unit">W/m²</span></td>
+        <td>kuun suurin <?php echo $solar_radiation_max ?><span class="unit">W/m²</span></td>
       </tr>
 
       <tr>
@@ -111,20 +165,32 @@
           UV-indeksi:
         </td>
         <td>
-          0
+          <?php echo $uv_index ?>
         </td>
-        <td>ei UV-säteilyä</td>
+        <td>
+          <?php
+          if($uv_index == 0){
+            echo "ei UV-säteilyä";
+          } elseif ($uv_index < 2.5){
+            echo "matala";
+          } elseif ($uv_index < 5.5){
+            echo "kohtalainen, suojaudu";
+          } else {
+            echo "korkea, suojaudu";
+          }
+           ?>
+        </td>
       </tr>
 
       <tr>
         <td></td>
         <td></td>
-        <td>Päivitetty 21.4.2019 16:21</td>
+        <td><?php echo $update_time ?></td>
       </tr>
     </table>
 
     <a href="./">
-      <div class="close material-icons button">close</div>
+      <i class="material-icons button close">close</i>
     </a>
 </body>
 </html>
