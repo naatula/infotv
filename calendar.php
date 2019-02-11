f894caafc1e7ae15c57cad403bb6cdac925caeb7
Simo Naatula <git@naatu.la>
2019-02-11 19:14:08 +0200

    Fix calendar view layout and remove CSS calculations

--- calendar.php
+++ calendar.php
@@ -8,7 +8,8 @@
 </head>
 <body id="calendar">
     <iframe src="https://www.google.com/calendar/embed?src=8khe2ngv0i2rvgpn58s20i331s%40group.calendar.google.com&ctz=Europe/Helsinki&hl=fi"></iframe>
-    <div id="calendar_bar"></div>
+    <div id="calendar_options_bar"></div>
+    <div id="calendar_footer_bar"></div>
     <div id="calendar_noclick"></div>
     <a href="./">
       <div class="close material-icons">close</div>
