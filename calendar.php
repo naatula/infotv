036b5048c3c20196afe9d754ed2fe1d598d24515
Simo Naatula <git@naatu.la>
2019-02-12 17:57:01 +0200

    Add overlay preventing unwanted clicks

--- calendar.php
+++ calendar.php
@@ -9,6 +9,7 @@
 <body class="calendar">
   <div id="calendar_title_bar">Tapahtumakalenteri</div>
   <div id="calendar_content">
+    <div id="calendar_noclick"></div>
     <iframe class="calendar" src="https://www.google.com/calendar/embed?src=8khe2ngv0i2rvgpn58s20i331s%40group.calendar.google.com&showTabs=0&showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;wkst=2&amp;hl=fi&amp;bgcolor=%23ffffff&amp;ctz=Europe%2FHelsinki"></iframe>
     <div id="calendar_footer_bar"></div>
   </div>
