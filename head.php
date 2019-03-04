68ea6c7c9cd5c2b549e10b75fc3993fd858ad79f
Simo Naatula <git@naatu.la>
2019-03-04 13:01:24 +0200

    Add logging

--- head.php
+++ head.php
@@ -10,6 +10,12 @@ $credentials = parse_ini_file('credentials.ini');
 
 $ip_whitelist = explode(" ", $credentials["ip_whitelist"]);
 $ip = $_SERVER['REMOTE_ADDR'];
+$time = date("Y-m-d\TH:i:s");
+$uri = str_replace(',', ' ', $_SERVER['REQUEST_URI']);
+
+file_put_contents('temp/log.csv', "$time,$ip,$uri".PHP_EOL , FILE_APPEND | LOCK_EX);
+
+$ip = $_SERVER['REMOTE_ADDR'];
 if(!empty($ip_whitelist[0]) && !in_array($ip, $ip_whitelist)){
   header('HTTP/1.0 403 Forbidden');
   echo "<meta http-equiv='refresh' content='600'>";
